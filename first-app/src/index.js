1e9d83c769444b1dc63f439e8e75f0dd3fa66386
SepehrShapouri <110922785+SepehrShapouri@users.noreply.github.com>
2023-04-04 12:29:48 +0330

    initial commit

--- first-app/src/index.js
+++ first-app/src/index.js
@@ -3,4 +3,5 @@ import React from "react";
 import App from "./components/App.js/App";
 import HookUser from "./components/HookUser/HookUser";
 import Shop from "./components/shop.js/Shop";
-ReactDOM.render(<App/>, document.querySelector("#root"));
+import ClassCounter from "./components/classCounter/ClassCounter";
+ReactDOM.render(<ClassCounter/>, document.querySelector("#root"));
